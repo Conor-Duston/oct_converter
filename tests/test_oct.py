bb0160237ab05db0e09ad9864a009a6625b2f2a8
Conor-Duston <conor.duston@gmail.com>
2024-03-17 16:48:01 -0400

    Added negative numbers to tests

--- tests/test_oct.py
+++ tests/test_oct.py
@@ -7,6 +7,6 @@ import pytest
 def liboct():
     yield CDLL("../bin/lib_oct_converter.so")
 
-@pytest.mark.parametrize("dec,oct", [(0, 0), (1, 1), (8, 10), (70, 106), (707, 1303)])
+@pytest.mark.parametrize("dec,oct", [(0, 0), (1, 1), (8, 10), (70, 106), (707, 1303), (-1, -1), (-8, -10), (-70, -106), (-707, -1303)])
 def test_oct_conv(liboct, dec : int, oct : int):
     assert liboct.dec_to_oct(dec) == oct
